8b8da6a9c2fa085d25d756a15f1894f9fe22d4a3
AlexVladimir <abuklov133.1@gmail.com>
2023-10-07 10:01:56 +0300

    6. Checkstyle [#4123]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -2,7 +2,6 @@ package ru.job4j;
 
 public class Info {
     public static void main(String[] args) {
-        System.out.println("07.10.23");
         System.out.println("Buklov Akexey");
         System.out.println("Выполнил настройку CheckStile");
     }
