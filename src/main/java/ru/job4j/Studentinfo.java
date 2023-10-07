777bb9931e977b2e861b566d8079f4fa4805cb48
AlexVladimir <abuklov133.1@gmail.com>
2023-10-07 08:41:52 +0300

    3.3. Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main(String[] args) {
+        System.out.println("Alexey Buklov");
+        System.out.println("07.03.1980");
+    }
+}
