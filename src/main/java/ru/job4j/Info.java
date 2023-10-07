d93b08ff1fb9b2a4b065f423c9af0ff6b52957ec
AlexVladimir <abuklov133.1@gmail.com>
2023-10-07 09:57:51 +0300

    4.2. Заголовок в файле java [#159036]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -4,5 +4,6 @@ public class Info {
     public static void main(String[] args) {
         System.out.println("07.10.23");
         System.out.println("Buklov Akexey");
+        System.out.println("Выполнил настройку CheckStile");
     }
 }
