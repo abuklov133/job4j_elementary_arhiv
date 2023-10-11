cdda8438ba3c6fdb45eb0bedee019343ece57eeb
AlexVladimir <abuklov133.1@gmail.com>
2023-10-11 14:12:22 +0300

    7. Расстояние между точками в системе координат [#188 #451163]

--- src/main/java/ru/job4j/condition/Greeting.java
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -2,11 +2,16 @@ package ru.job4j.condition;
 
 public class Greeting {
     public static void main(String[] args) {
-        String idea = "I like Java!";
+      /*  String idea = "I like Java!";
         System.out.println(idea);
         idea += "But I am a newbie.";
         int year = 2023;
         idea += year;
         System.out.println(idea);
+
+       */
+        double d = 121.19;
+        byte b = (byte) d;
+        System.out.println(d);
     }
 }
