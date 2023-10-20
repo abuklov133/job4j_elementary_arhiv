29dd8a263fd0245041207b5261ebad0d64ce22ce
AlexVladimir <abuklov133.1@gmail.com>
2023-10-20 22:36:44 +0300

    6.5. Поиск индекса в диапазоне. [#156318]

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -11,4 +11,20 @@ public class FindLoop {
         }
         return rst;
     }
+
+    public static int indexInRange(int[] data, int el, int start, int finish) {
+        int rst = -1;
+        for (int i = start; i <= finish; i++) {
+            if (data[i] == el) {
+               rst = i;
+               break;
+            }
+        }
+        return rst;
+    }
+
+    public static void main(String[] args) {
+        int[] data = {0, 45, 2, 46, 7, 5};
+        System.out.println(indexInRange(data, 7, 0, 4));
+    }
 }
--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -8,7 +8,7 @@ class FindLoopTest {
 
     @Test
     void whenArrayHas5Then0() {
-        int[] data = new int[] {5, 10, 3};
+        int[] data = new int[]{5, 10, 3};
         int el = 5;
         int result = FindLoop.indexOf(data, el);
         int expected = 0;
@@ -17,10 +17,32 @@ class FindLoopTest {
 
     @Test
     void whenArrayHasNot7ThenMinus1() {
-        int[] data = new int[] {5, 10, 3};
+        int[] data = new int[]{5, 10, 3};
         int el = 7;
         int result = FindLoop.indexOf(data, el);
         int expected = -1;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenDataRangeHasElThenIndex() {
+        int[] data = {0, 1, 2, 3, 4, 5};
+        int el = 3;
+        int start = 1;
+        int finish = 4;
+        int expected = 3;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenDataRangeHasNotElThenMinusOne() {
+        int[] data = {0, 1, 2, 3, 4, 5};
+        int el = 5;
+        int start = 0;
+        int finish = 4;
+        int expected = -1;
+        int result = FindLoop.indexInRange(data, el, start, finish);
         assertThat(result).isEqualTo(expected);
     }
 }
